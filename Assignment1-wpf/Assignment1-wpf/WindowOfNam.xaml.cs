d050cd6991734b1a2fb950695ccaa2b23eeb4b16
devnam1401 <namphhe180085@fpt.edu.vn>
2026-01-27 16:08:41 +0700

    tam biet

--- Assignment1-wpf/Assignment1-wpf/WindowOfNam.xaml.cs
+++ Assignment1-wpf/Assignment1-wpf/WindowOfNam.xaml.cs
@@ -27,5 +27,11 @@ namespace Assignment1_wpf
         {
             MessageBox.Show("xin chào");
         }
+
+
+        private void btnBye_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("tạm biệt");
+        }
     }
 }
